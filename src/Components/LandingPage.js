41c3f34d4f7a0f9705c564842a3c3880f0edd032
Tagbo Chinedu <johntagbo2@gmail.com>
2022-05-15 16:59:02 +0100

    third commit

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,51 +1,80 @@
+import { useState } from "react";
+
 const LandingPage = () => {
+  const [nav, setNav] = useState(true);
+  const next = ">";
+  const menuChangeHandler = () => {
+    setNav(!nav);
+  };
+
   return (
     <section>
-      <nav class="relative w-full flex flex-wrap items-center justify-between py-3 px-12 bg-login-blue text-gray-200 navbar navbar-expand-lg navbar-light">
-        <div class="container-fluid w-full items-center justify-between px-6">
-          <ul class="navbar-nav flex pl-0 list-style-none mr-auto">
-            <li class="nav-item p-2">
-              <a class="nav-link text-white" href="facebook.com">
-                Jobs
-              </a>
-            </li>
-            <li class="nav-item p-2">
-              <a class="nav-link text-white" href="facebook.com">
-                Company Review
-              </a>
-            </li>
-            <li class="nav-item p-2">
-              <a
-                class="nav-link text-white opacity-60 hover:opacity-80 focus:opacity-80 p-0"
-                href="facebook.com"
-              >
-                Find Salaries
-              </a>
-            </li>
-            <li class="nav-item p-2">
-              <a
-                class="nav-link text-white opacity-60 hover:opacity-80 focus:opacity-80 p-0 "
-                href="facebook.com"
-              >
-                Post Jobs
-              </a>
-            </li>
-          </ul>
+      <nav class="w-full items-center py-6 lg:px-12 px-2 bg-login-blue text-white">
+        <div className="flex justify-between">
+          <div className="-skew-y-17 font-bold text-xl">
+            <a href="home">Find Jobs</a>
+          </div>
+          <div class="items-center lg:px-6 px-2 relative">
+            <span
+              className="lg:hidden absolute right-2 transition-all duration-500 ease-linear"
+              onClick={menuChangeHandler}
+            >
+              <ion-icon
+                size="large"
+                name={nav ? "menu-outline" : "close-outline"}
+              ></ion-icon>
+            </span>
+            <ul
+              class={`${"navbar-nav lg:flex pl-0 list-style-none mr-auto transition-all duration-500 ease-linear "} ${
+                nav ? "hidden" : "block "
+              }`}
+            >
+              <li class="nav-item p-2">
+                <a class="nav-link text-white" href="">
+                  Jobs
+                </a>
+              </li>
+              <li class="nav-item p-2">
+                <a class="nav-link text-white" href="">
+                  Company Review
+                </a>
+              </li>
+              <li class="nav-item p-2">
+                <a class="nav-link text-white" href="">
+                  Find Salaries
+                </a>
+              </li>
+              <li class="nav-item p-2">
+                <a
+                  class="nav-link text-white p-2 rounded font-bold border-1 bg-white text-login-blue "
+                  href=""
+                >
+                  Post Jobs
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
         <div>
-          <h1 class="text-4xl my-20 mx-12">Find Your Dream Job</h1>
+          <h1 class="text-4xl my-20 md:mx-12">Find Your Dream Job</h1>
         </div>
       </nav>
-      <div class="px-16 py-20">
-        <div class="flex w-1/3 justify-between">
+      <div class="lg:px-16 py-20 md:px-0 relative">
+        <div className="lg:w-4/5 w-full flex -top-5 absolute px-8 lg:mx-16 focus:border-none text-slate-500 ">
+          <form className=" z-10 justify center w-full ">
+            <input className="w-full  h-10 border-2 rounded-lg " />
+          </form>
+        </div>
+
+        <div class="flex md:w-5/12 sm:w-full justify-between">
           <p>Showing 68 results</p>
           <p>
             <span class="text-slate-400">Sort by:</span> Latest
           </p>
         </div>
-        <div class="flex justify-between">
-          <div class="w-5/12">
-            <div class="bg-login-blue text-white border-2 p-5 mb-10 rounded-xl shadow-2xl">
+        <div class="md:flex sm:block lg:justify-between md:justify-around">
+          <div class="md:w-5/12 sm:w-full">
+            <div class=" bg-login-blue text-white border-2 p-5 lg:mb-10 md:mb-0 lg:rounded-xl shadow-2xl">
               <div class="flex justify-between ">
                 <h1 class="text-xl pb-5 font-bold">Front end developer</h1>
                 <h1 class="text-xl pb-5 font-bold">$2k-$5k</h1>
@@ -57,16 +86,14 @@ const LandingPage = () => {
                 skills
               </p>
               <div class="text-right">
-                <button class="text-red-400 bg-white border-1 rounded-xl text-md my-5 p-1">
+                <button class="text-red-400 bg-white border-1 lg:rounded-xl text-md my-5 p-1">
                   See more
                 </button>
               </div>
             </div>
-            <div class="bg-white text-slate-500 border-2 p-5 mb-10 rounded-xl shadow-2xl">
+            <div class="bg-white text-slate-500 border-2 p-5 lg:mb-10 md:mb-0 lg:rounded-xl shadow-2xl">
               <div class="flex justify-between text-login-blue">
-                <h1 class="text-xl  pb-5 font-bold">
-                  Front end developer
-                </h1>
+                <h1 class="text-xl  pb-5 font-bold">Front end developer</h1>
                 <h1 class="text-xl pb-5 font-bold">$3k-$7k</h1>
               </div>
               <h3>Yaba, Lagos</h3>
@@ -75,16 +102,14 @@ const LandingPage = () => {
                 back-end developers and with front end web technologies
               </p>
               <div class="text-right">
-                <button class="bg-red-400 text-white border-1 rounded-xl text-md my-5 p-1">
+                <button class="bg-red-400 text-white border-1 lg:rounded-xl text-md my-5 p-1">
                   See more
                 </button>
               </div>
             </div>
-            <div class="bg-white text-slate-500 border-2 p-5 mb-10 rounded-xl shadow-2xl">
+            <div class="bg-white text-slate-500 border-2 p-5 lg:mb-10 md:mb-0 lg:rounded-xl shadow-2xl">
               <div class="flex justify-between text-login-blue">
-                <h1 class="text-xl  pb-5 font-bold">
-                  Front end developer
-                </h1>
+                <h1 class="text-xl  pb-5 font-bold">Front end developer</h1>
                 <h1 class="text-xl pb-5 font-bold">$4k-$8k</h1>
               </div>
               <h3>Victoria Island, Lagos</h3>
@@ -94,77 +119,77 @@ const LandingPage = () => {
                 and/or React.js and everything in between
               </p>
               <div class="text-right">
-                <button class="bg-red-400 text-white border-1 rounded-xl text-md my-5 p-1">
+                <button class="bg-red-400 text-white border-1 lg:rounded-xl text-md my-5 p-1">
                   See more
                 </button>
               </div>
             </div>
-            <div class="bg-white text-slate-500 border-2 p-5 mb-10 rounded-xl shadow-2xl">
+            <div class="bg-white text-slate-500 border-2 p-5 lg:mb-10 md:mb-0 lg:rounded-xl shadow-2xl">
               <div class="flex text-login-blue justify-between font-bold">
-                <h1 class="text-xl pb-5">
-                  Front end developer
-                </h1>
+                <h1 class="text-xl pb-5">Front end developer</h1>
                 <h1 class="text-xl pb-5 ">$5k-$10k</h1>
               </div>
               <h3>Lekki, Lagos</h3>
               <p>
                 In this role, you will be responsible for developing an
-                implementing user interface components using React.js cncepts
+                implementing user interface components using React.js concepts
                 and workflows such as Redux, Flux and Webpack.
               </p>
               <div class="text-right">
-                <button class="bg-red-400 text-white border-1 rounded-xl text-md my-5 p-1">
+                <button class="bg-red-400 text-white border-1 lg:rounded-xl text-md my-5 p-1">
                   See more
                 </button>
               </div>
             </div>
           </div>
-          <div class="h-full w-5/12 p-5 border-2 rounded-xl shadow-2xl">
+          <div class="h-full md:w-5/12 md:block sm:hidden p-5 border-2 lg:rounded-xl shadow-2xl">
             <div class="border-b-2 py-6">
-              <h1 class="text-xl pb-5 font-bold text-login-blue">Front end developer</h1>
+              <h1 class="text-xl pb-5 font-bold text-login-blue">
+                Front end developer
+              </h1>
               <button class="bg-login-blue border-2 rounded-md p-1 text-white">
                 Apply Via Find Job
               </button>
             </div>
             <div class="py-6 text-lg text-slate-500">
               <p class="pb-2">
-                In this role, you will be responsible fr developing and
+                In this role, you will be responsible for developing and
                 implementing user interface components using React.js concepts
                 and workflow such as Redux, Flux and Wwebpack. You will also be
-                respnsible for profiling an dimproving front end performance and
+                respnsible for profiling and improving front end performance and
                 documenting our front-end codebase
               </p>
               <ul class="list-disc pl-6">
-                <li class='py-1'>Minimum Qualification: Degree</li>
-                <li class='py-1'>Experience Level: Senior Level</li>
-                <li class='py-1'>Experience Length: 5years</li>
+                <li class="py-1">Minimum Qualification: Degree</li>
+                <li class="py-1">Experience Level: Senior Level</li>
+                <li class="py-1">Experience Length: 5years</li>
               </ul>
-              <h3 class='font-bold'>Job Descripion/Requirements</h3>
+              <h3 class="font-bold">Job Descripion/Requirements</h3>
               <ul class="list-disc pl-6">
-                <li class='py-3'>
+                <li class="py-3">
                   5+ years experience of front end related(HTML5 +JS + CSS3)
                   development work experience, familiar with mobile application
                   development
                 </li>
-                <li class='py-1'>
+                <li class="py-1">
                   Proficient in Javascript/Typescript, mastering technologies
                   such as HTML, CSS, DOM, AJAX, etc, can quickly complete the
                   established interactive design functions; Atleast 4 years
                   experience is required
                 </li>
-                <li class='py-1'>
+                <li class="py-1">
                   Through understanding of React.js and its core principles
                 </li>
-                <li class='py-1'>
+                <li class="py-1">
                   Experience with popular React.js workflows (such as Flux or
                   Redux)
                 </li>
-                <li class='py-1'>Familiarity with RESTful API's</li>
-                <li class='py-1'>Knowledge of isomorphic react is a plus</li>
-                <li class='py-1'>
+                <li class="py-1">Familiarity with RESTful API's</li>
+                <li class="py-1">Knowledge of isomorphic react is a plus</li>
+                <li class="py-1">
                   Familiaritywith modern front-end build pipelines and tools
                 </li>
-                <li class='py-1'>
+                <li class="py-1">
                   Experience with common front-end development tools such as
                   Babel, Webpack, NPM, etc
                 </li>
@@ -172,12 +197,32 @@ const LandingPage = () => {
             </div>
           </div>
         </div>
+        <div className="pt-5">
+          <button className="px-2 py-1 text-white font-bold bg-login-blue border-2">
+            1
+          </button>
+          <button className="px-2 py-1 mx-1 text-slate-500 font-bold border-2">
+            2
+          </button>
+          <button className="px-2 py-1 mx-1 text-slate-500 font-bold border-2">
+            3
+          </button>
+          <button className="px-2 py-1 mx-1 text-slate-500 font-bold border-2">
+            4
+          </button>
+          <button className="px-2 py-1 mx-1 text-slate-500 font-bold border-2">
+            5
+          </button>
+          <button className="px-2 py-1 mx-1 text-slate-500 font-bold border-2">
+            {next}
+          </button>
+        </div>
       </div>
-      <footer class="flex bg-login-blue text-white px-12 justify-between">
-        <div class="w-1/5">
-          <h1>Find Jobs</h1>
+      <footer class="flex bg-login-blue text-white px-12 py-8 justify-between">
+        <div className="w-1/5 text-bold">
+          <a href="home">Find Jobs</a>
         </div>
-        <div class="w-1/5 text-lg">
+        <div class="w-1/5 text-md lg:block xs:hidden">
           <h3>Quick Links</h3>
           <ul>
             <li>Home</li>
@@ -186,7 +231,7 @@ const LandingPage = () => {
             <li>Term and Conditions</li>
           </ul>
         </div>
-        <div class="w-1/5 text-lg">
+        <div class="w-1/5 text-md">
           <h3>Quick Links</h3>
           <ul>
             <li>Home</li>
@@ -195,7 +240,7 @@ const LandingPage = () => {
             <li>Term and Conditions</li>
           </ul>
         </div>
-        <div class="w-1/5 text-center text-lg">
+        <div class="w-1/5 text-center text-md">
           <h1>Social Media</h1>
         </div>
       </footer>
---
+++ src/Components/Register/SignIn.js
@@ -0,0 +1,108 @@
+import { useRef } from "react";
+import { useNavigate } from "react-router-dom";
+
+import Image from "../../Resources/login.jpg";
+
+const SignIn = (props) => {
+  //Hook
+  const navigate = useNavigate();
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDARs0gHPZ6ei0wmSMBZTOFXzrC7_chR4U",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          email: enteredEmail,
+          password: enteredPassword,
+          returnSecureToken: true,
+        }),
+        headers: { "Content-Type": "application/json" },
+      }
+    ).then((res) => {
+      if (res) {
+        props.signIn(res.ok);
+        navigate("home");
+      } else {
+        res.json().then((data) => {
+          let errorMessage = "Authentication failed";
+          if (data && data.error && data.error.message) {
+            errorMessage = data.error.message;
+          }
+          alert(errorMessage);
+        });
+      }
+    });
+  };
+
+  return (
+    <section className="h-screen">
+      <div className=" h-full">
+        <div className="lg:flex sm:block xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6  ">
+          <div className="lg:relative lg:w-5/12 xs:w-full lg:h-full">
+            <div className="-skew-x-12 bg-login-blue hidden lg:block absolute z-10 w-2/4 -right-16 h-full"></div>
+            <div className="-skew-x-17 bg-red-300 hidden lg:block absolute z-0 w-1/4 -right-24 h-full"></div>
+            <div className="z-20 lg:absolute lg:w-full mb-12 md:mb-0 bg-login-blue lg:pl-20 sm:p-5 lg:h-full ">
+              <h1 className="g:text-3xl text-2xl w-full lg:max-w-xs pt-2 pb-2 text-white font-bold">
+                Find the best candidates for your organisation
+              </h1>
+              <img
+                src={Image}
+                className="w-full pt-0 lg:block hidden"
+                alt="two people at a table"
+              />
+            </div>
+          </div>
+
+          <div className="lg:w-5/12 sm:w-full  mb-12 py-2 px-6 ">
+            <form onSubmit={submitHandler}>
+              <div className="flex flex-row items-center justify-center lg:justify-start text-gray-800">
+                <p className="text-lg mb-0 mr-4">Sign In</p>
+              </div>
+              <div className="my-6">
+                <input
+                  type="text"
+                  className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                  placeholder="Email address"
+                  ref={emailInputRef}
+                />
+              </div>
+              <div className="mb-6">
+                <input
+                  type="password"
+                  className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                  placeholder="Password"
+                  ref={passwordInputRef}
+                />
+              </div>
+
+              <div className="text-center lg:text-left">
+                <button
+                  type="submit"
+                  className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                >
+                  Sign In
+                </button>
+                <p className="block text-slate-500 mt-8">
+                  Don't have an account? Sign up{" "}
+                  <a href="signup" className="text-red-400 font-bold">
+                    Here
+                  </a>
+                </p>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SignIn;
